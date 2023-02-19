1f9d443f9302b2bea13914991e1175f063339af0
Vishal1732 <92910196+Vishal1732@users.noreply.github.com>
2023-02-19 16:39:54 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,25 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App";
+// import '../styles/App.css';
+const App = () => {
+  const handleInput = (event) => {
+    // use console.log
+    console.log(`Input in #${event.target.id} is ${event.target.value}`);
+    // #text-input is sfdfg
+    // #num-input is 83247
+  };
 
+  // do not change id of input elements
+  return (
+    <div id="main">
+      <label htmlFor="text-input">Text Input:- </label>
+      <input id="text-input" onChange={handleInput} type={"text"} />
+      <br />
+      <br />
+      <label htmlFor="num-input">Number input</label>
+      <input id="num-input" onChange={handleInput} type={"number"} />
+      <br />
+    </div>
+  );
+};
 
-
-ReactDOM.render(<App />, document.getElementById("root"));
+export default App;
